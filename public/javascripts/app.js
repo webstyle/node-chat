c834a50ef0328e02b4507938e6b0eec115324f71
Farrukh Mamatkhalilov <webstyle@protonmail.com>
2015-11-14 12:53:18 +0500

    UI to'g'irladim

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,7 +4,7 @@ var appUrl = 'http://localhost:8080/api/v1';
 /**
  * Message Controller.
  */
-NodeChat.controller('MsgCtrl', function($scope, $http) {
+NodeChat.controller('MsgCtrl', function($scope, $http, $anchorScroll, $location) {
 
   /**
    * Get messages list.
@@ -14,25 +14,34 @@ NodeChat.controller('MsgCtrl', function($scope, $http) {
       console.log('Message data response', data);
       $scope.messages = data;
     });
+    $scope.gotoBottom();
   }
+
+  $scope.gotoBottom = function() {
+    $location.hash('bottom');
+    $anchorScroll();
+  };
 
   $scope.loadMessages();
 
   /**
    * Delete message by id.
    */
-   $scope.deleteMessage = function (id) {
-     $http.delete(appUrl + '/message/' + id).success(function (data) {
-       $scope.loadMessages();
-       console.log(data);
-     });
-   }
+  $scope.deleteMessage = function(id) {
+    $http.delete(appUrl + '/message/' + id).success(function(data) {
+      $scope.loadMessages();
+      console.log(data);
+    });
+  }
 
   /**
    * Save messages list.
    */
   $scope.save = function() {
-    $http.post(appUrl + '/message', {content:$scope.content, user:'Farrukh'}).success(function(data) {
+    $http.post(appUrl + '/message', {
+      content: $scope.content,
+      user: 'Farrukh'
+    }).success(function(data) {
       console.log('Message save data response');
       $scope.loadMessages();
       $scope.content = null;
